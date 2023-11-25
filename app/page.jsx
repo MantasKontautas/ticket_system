51764efb2ec9be1a9e326511958ad96b808e975e
MantasKontautas <kontautas.mantas@gmail.com>
2023-11-25 08:47:03 +0100

    update description in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,24 @@
-import Image from "next/image";
 import Form from "./components/Form";
 
 export default function Home() {
   return (
     <main>
-      <div className="mt-11 w-4/5 m-auto">
+      <div className="mt-11 w-4/5 m-auto flex flex-col items-center text-center">
         {/* Introduction */}
-        <div className="mt-20">
-          <h1 className="font-bold text-center text-3xl">
-            Event Ticket System
-          </h1>
-          <h4 className="text-center">Demo</h4>
-          <p className="text-center mt-20">
-            No actual money will be spent. Please type in your event data and
-            press Generate.
-          </p>
-        </div>
+
+        <h1 className="font-bold text-3xl">Event Ticket System</h1>
+        <h4>Demo</h4>
+        <a
+          href="https://github.com/MantasKontautas/ticket_system"
+          className="underline"
+        >
+          https://github.com/MantasKontautas/ticket_system
+        </a>
+        <p className="mt-10 max-w-[30rem]">
+          No actual money will be spent. Type in your mock event data and
+          press Generate. Then, you will be redirected to checkout. Input the
+          demo card 4242 4242 4242 4242. Other data can be anything.
+        </p>
       </div>
 
       <Form />
